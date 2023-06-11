4a4ee4ee600522a991dce2ee5e98dec255937d04
Adrian Paszek <adrian.paszek@gmail.com>
2023-06-11 18:29:45 +0200

    boards api

--- borads_api.py
+++ borads_api.py
@@ -1,3 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, make_response
+from data_manager import execute_select, execute_insert
+
 boards_bp = Blueprint('boards_api', __name__)
+
+@boards_bp.route("/boards")
+def get_boards():
+    result = execute_select("SELECT * FROM boards")
+    return jsonify(result)
+
+@boards_bp.route('/board', methods=['POST'])
+def add_board():
+    data = request.get_json()
+
+    if not data or 'name' not in data:
+        return make_response(jsonify({'error' : 'Bad Request (board name not specified)'}))
+    execute_insert('INSERT INTO boards (title) VALUES (%s)', (data['name'],))
+    return jsonify({'message' : f'Submitted board with name {data["name"]}'})
+
 
